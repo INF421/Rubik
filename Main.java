1fe8c53155d3218c3a45ec1dcc94d36f8d27b677
Ellongised <pub.ellongised@gmail.com>
2014-12-16 13:04:45 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cube test = new Cube("000000000222111111333222222444333333111444444555555555");
+//		int n = 1;
+//		for (int i = 0 ; i < 8 ; i++) {
+//			long t0 = System.currentTimeMillis();
+//			test.melange(n);
+//			long tf = System.currentTimeMillis();
+//			System.out.println(n);
+//			System.out.println(tf-t0);
+//			n *= 10;
+//		}
+//		System.out.println(test.estResolu());
+//		test.combinaison("0132412351");
+//		System.out.println(test.estResolu());
+//		test.combinaison("5321433203");
+//		System.out.println(test.estResolu());
+//		test.rotate(0, -2);
+//		System.out.println(test.estResolu());
+		test.display();
+	}
+
+}
